42346ea02da3fa03345dbe60f9824415855050ff
Vishal Singhania <vishsinghania@outlook.com>
2024-03-28 18:11:49 +0530

    Initial commit

---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,34 @@
+if status is-interactive
+	# starship config
+	if test $TERM = 'xterm-kitty'
+		starship init fish | source
+	end
+	
+	# zoxide source
+	zoxide init --cmd cd fish | source
+	
+	# lsd
+	alias ls="lsd"
+	alias lsl="lsd --long"
+	alias lsa="lsd --all"
+	alias lsla="lsd --long --all"
+	alias lst="lsd --tree"
+	
+	# rebos
+	alias rgc="rebos gen commit"
+	alias rgl="rebos gen list"
+	alias rgb="rebos gen current build"
+	
+	# yay
+	alias pacre='yay -Rns' # uninstall a package
+	alias pacin='yay -S --needed' # install a package
+	alias pacup='yay -Syu' # update system
+	alias pacls='yay -Qs' # search installed package
+	alias paclsf='yay -Qm' # search AUR installed package
+	alias pacs='yay -Ss' # search availabe package
+	alias pacc='yay -Sc' # remove unused cache
+	alias pacro='yay -Qdtq | yay -Rn -' # remove unused dependencies
+
+	# Always mkdir a path
+	abbr mkdir 'mkdir -p'
+end
---
+++ dot_config/private_fish/functions/fish_greeting.fish
@@ -0,0 +1,6 @@
+if test $TERM = 'xterm-kitty'
+	pokemon-colorscripts --no-title --random
+	uptime --pretty
+else
+	uptime --pretty
+end
