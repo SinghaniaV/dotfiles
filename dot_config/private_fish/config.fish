3db24a1f835efb276c46e86bc6c8706ee769779b
Vishal Singhania <vishsinghania@outlook.com>
2024-03-30 23:35:13 +0530

    Update .config/fish/config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -28,7 +28,7 @@ if status is-interactive
 	alias pacls='yay -Qs' # search installed package
 	alias paclsf='yay -Qm' # search AUR installed package
 	alias paclso='yay -Qdtq' # search orphaned dependencies
-	alias pacs='yay -Ss' # search availabe package
+	alias pacs='yay -Ss' # search available package
 	alias pacc='yay -Sc' # remove unused cache
 	alias pacro='yay -Qdtq | yay -Rn -' # remove orphaned dependencies
 
