3ac79140f82f0ac54daafac9fadadbee0a5fb91d
Vishal Singhania <vishsinghania@outlook.com>
2024-08-06 02:40:16 +0530

    Update .config/fish/config.fish Update .config/rebos/machines/thinkpadx1/gen.toml

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -4,6 +4,19 @@ if status is-interactive
 	starship init fish | source
 	enable_transience
 	
+	# >>> conda initialize >>>
+	# !! Contents within this block are managed by 'conda init' !!
+	if test -f /opt/miniconda3/bin/conda
+		eval /opt/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+	else
+		if test -f "/opt/miniconda3/etc/fish/conf.d/conda.fish"
+		    . "/opt/miniconda3/etc/fish/conf.d/conda.fish"
+		else
+		    set -x PATH "/opt/miniconda3/bin" $PATH
+		end
+	end
+	# <<< conda initialize <<<
+
 	# zoxide source
 	zoxide init --cmd cd fish | source
 
